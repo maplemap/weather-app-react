46987c44e16ea96d19179da488b0e510f20d1c6c
Serhii Illarionov <sergio.illarionov@gmail.com>
2024-06-16 16:34:30 +0300

    add: loader component

---
+++ src/ui-kit/loader/loader.tsx
@@ -0,0 +1,15 @@
+import './loader.scss';
+
+export const Loader = () => {
+  return (
+    <div>
+      <div className="loader-overlay">
+        <div className="spinner">
+          <div className="bounce1"></div>
+          <div className="bounce2"></div>
+          <div className="bounce3"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
