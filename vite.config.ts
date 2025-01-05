4d0980dc609fd64414d112c5d6655fa65f0b4818
Serhii Illarionov <sergio.illarionov@gmail.com>
2025-01-05 20:16:25 +0200

    update: deploy config with relative path to assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
